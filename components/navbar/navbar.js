0a708f6f8d92d240b32f50b8183a2c8b3d7f76c4
Jose-Antonio Rubio <jadrubio@gmail.com>
2022-05-19 12:09:42 -0400

    created navbar component with routing set up

---
+++ components/navbar/navbar.js
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './navbar.module.css';
+
+const NavBar = ({ username }) => {
+  const [dropDownOpen, setDropdownOpen] = useState(false);
+  const router = useRouter();
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push('/');
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push('/browse/my-list');
+  };
+
+  const toggleDropDown = (e) => {
+    e.preventDefault();
+    setDropdownOpen(!dropDownOpen);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>
+            <Image
+              src="/static/logo.webp"
+              alt="retro logo"
+              width="300px"
+              height="150px"
+            />
+          </div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn} onClick={toggleDropDown}>
+              <p className={styles.username}>{username}</p>
+              <Image
+                src="/static/expand_more.svg"
+                alt="expand more"
+                width="24px"
+                height="24px"
+              />
+            </button>
+            {dropDownOpen && (
+              <div className={styles.navDropdown}>
+                <div>
+                  <Link href="/login">
+                    <a className={styles.linkName}>Sign Out</a>
+                  </Link>
+                  <div className={styles.lineWrapper}></div>
+                </div>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
